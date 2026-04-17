0683a480f45db2b8b09d57932bc5294b256bb795
Kadeema Wakha <kadeemawakha@gmail.com>
2026-04-17 14:14:23 -0400

    Added to main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "student.h"
+#include "operations.h"
+
+void menu()
+{
+	printf("\n===== Student Grade Manager =====\n");
+	printf("1. Add Student\n");
+	printf("2. Remove Student\n");
+	printf("3. Search Student by ID\n");
+	printf("4. Add Grade to Student\n");
+	printf("5. Display All Students\n");
+	printf("6. Exit\n");
+	printf("Enter your choice: ");
+}
